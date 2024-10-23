fbf49b53399b5b1280ff18276a8072f0b548407d
superdarki <superdarki@proton.me>
2024-10-23 12:27:42 +0200

    allow all cross origin

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ app = FastAPI(root_path=ROOT_PATH)
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
